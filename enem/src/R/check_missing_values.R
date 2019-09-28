7d80793320bec2c6f837f2bfcbccf51001ac28a6
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-09-28 14:24:33 -0300

    Finish data pre-processing
    * Generate data file for R
    * Filter useful data

--- enem/src/R/check_missing_values.R
+++ enem/src/R/check_missing_values.R
@@ -1,10 +1,8 @@
 # Load
-enem <- read.csv('../../data/enem_data_converted.csv', stringsAsFactors = FALSE)
-enem[enem==""] = NA
+# enem <- read.csv('../../data/enem_data_converted.csv', stringsAsFactors = FALSE)
+enem <- readRDS("../../data/enem_filtered_data.rds")
 
-# remove NA NU_IDADE - 
-# enem[is.na(enem$NU_IDADE)] <- NULL
-enem <- enem[!(is.na(enem$TP_SEXO)), ]
+enem <- enem[!(is.na(enem$NU_IDADE)), ]
 
 # Add default values to missing fields
 enem$TP_ESTADO_CIVIL[is.na(enem$TP_ESTADO_CIVIL)] = 0           # 'solteiro'
@@ -29,45 +27,52 @@ enem$NU_NOTA_COMP5[is.na(enem$NU_NOTA_COMP5)] = 0
 
 enem$NU_NOTA_REDACAO[is.na(enem$NU_NOTA_REDACAO)] = 0           # essay grade
 
-enem$Q001[is.na(enem$Q001)] = 'H'                               # 'nao sei'
-enem$Q002[is.na(enem$Q002)] = 'H'                               # 'nao sei'
-enem$Q003[is.na(enem$Q003)] = 'F'                               # 'nao sei'
-enem$Q004[is.na(enem$Q004)] = 'F'                               # 'nao sei'
-enem$Q005[is.na(enem$Q005)] = 0                                 # undefined (new category)
-enem$Q006[is.na(enem$Q006)] = 'Z'                               # undefined (new category)
-enem$Q007[is.na(enem$Q007)] = 'Z'                               # undefined (new category)
-enem$Q008[is.na(enem$Q008)] = 'Z'                               # undefined (new category)
-enem$Q009[is.na(enem$Q009)] = 'Z'                               # undefined (new category)
-enem$Q010[is.na(enem$Q010)] = 'Z'                               # undefined (new category)
-enem$Q011[is.na(enem$Q011)] = 'Z'                               # undefined (new category)
-enem$Q012[is.na(enem$Q012)] = 'Z'                               # undefined (new category)
-enem$Q013[is.na(enem$Q013)] = 'Z'                               # undefined (new category)
-enem$Q014[is.na(enem$Q014)] = 'Z'                               # undefined (new category)
-enem$Q015[is.na(enem$Q015)] = 'Z'                               # undefined (new category)
-enem$Q016[is.na(enem$Q016)] = 'Z'                               # undefined (new category)
-enem$Q017[is.na(enem$Q017)] = 'Z'                               # undefined (new category)
-enem$Q018[is.na(enem$Q018)] = 'Z'                               # undefined (new category)
-enem$Q019[is.na(enem$Q019)] = 'Z'                               # undefined (new category)
-enem$Q020[is.na(enem$Q020)] = 'Z'                               # undefined (new category)
-enem$Q021[is.na(enem$Q021)] = 'Z'                               # undefined (new category)
-enem$Q022[is.na(enem$Q022)] = 'Z'                               # undefined (new category)
-enem$Q023[is.na(enem$Q023)] = 'Z'                               # undefined (new category)
-enem$Q024[is.na(enem$Q024)] = 'Z'                               # undefined (new category)
-enem$Q025[is.na(enem$Q025)] = 'Z'                               # undefined (new category)
-enem$Q026[is.na(enem$Q026)] = 'Z'                               # undefined (new category)
-enem$Q034[is.na(enem$Q034)] = 9                                 # undefined (new category)
-enem$Q035[is.na(enem$Q035)] = 9                                 # undefined (new category)
-enem$Q036[is.na(enem$Q036)] = 9                                 # undefined (new category)
-enem$Q037[is.na(enem$Q037)] = 9                                 # undefined (new category)
-enem$Q038[is.na(enem$Q038)] = 9                                 # undefined (new category)
-enem$Q039[is.na(enem$Q039)] = 9                                 # undefined (new category)
-enem$Q040[is.na(enem$Q040)] = 9                                 # undefined (new category)
-enem$Q042[is.na(enem$Q042)] = 'Z'                               # undefined (new category)
-enem$Q043[is.na(enem$Q043)] = 'Z'                               # undefined (new category)
-enem$Q044[is.na(enem$Q044)] = 'Z'                               # undefined (new category)
-enem$Q045[is.na(enem$Q045)] = 'Z'                               # undefined (new category)
-enem$Q046[is.na(enem$Q046)] = 'Z'                               # undefined (new category)
-enem$Q047[is.na(enem$Q047)] = 'Z'                               # undefined (new category)
-enem$Q048[is.na(enem$Q048)] = 'Z'                               # undefined (new category)
-enem$Q049[is.na(enem$Q049)] = 'Z'                               # undefined (new category)
-enem$Q050[is.na(enem$Q050)] = 'Z'                               # undefined (new category)
+enem$Q001[enem$Q001 == ''] = 'H'                                # 'nao sei'
+enem$Q002[enem$Q002 == ''] = 'H'                                # 'nao sei'
+enem$Q003[enem$Q003 == ''] = 'F'                                # 'nao sei'
+enem$Q004[enem$Q004 == ''] = 'F'                                # 'nao sei'
+
+# Remove rows that have too few empty fields
+# It is not necessary to create a new category for these rows,
+# we can just remove them as there are just a few
+enem <- enem[!(is.na(enem$Q005)), ]
+
+# Unnecessary to delete rows for other Qs as deleting the rows for Q005 already
+# deletes those
+# enem <- enem[!(enem$Q006 == '' ), ]
+# enem <- enem[!(enem$Q007 == '' ), ]
+# enem <- enem[!(enem$Q008 == '' ), ]
+# enem <- enem[!(enem$Q009 == '' ), ]
+# enem <- enem[!(enem$Q010 == '' ), ]
+# enem <- enem[!(enem$Q011 == '' ), ]
+# enem <- enem[!(enem$Q012 == '' ), ]
+# enem <- enem[!(enem$Q013 == '' ), ]
+# enem <- enem[!(enem$Q014 == '' ), ]
+# enem <- enem[!(enem$Q015 == '' ), ]
+# enem <- enem[!(enem$Q016 == '' ), ]
+# enem <- enem[!(enem$Q017 == '' ), ]
+# enem <- enem[!(enem$Q018 == '' ), ]
+# enem <- enem[!(enem$Q019 == '' ), ]
+# enem <- enem[!(enem$Q020 == '' ), ]
+# enem <- enem[!(enem$Q021 == '' ), ]
+# enem <- enem[!(enem$Q022 == '' ), ]
+# enem <- enem[!(enem$Q023 == '' ), ]
+# enem <- enem[!(enem$Q024 == '' ), ]
+# enem <- enem[!(enem$Q025 == '' ), ]
+# enem <- enem[!(enem$Q026 == '' ), ]
+# enem <- enem[!(is.na(enem$Q034)), ]
+# enem <- enem[!(is.na(enem$Q035)), ]
+# enem <- enem[!(is.na(enem$Q036)), ]
+# enem <- enem[!(is.na(enem$Q037)), ]
+# enem <- enem[!(is.na(enem$Q038)), ]
+# enem <- enem[!(is.na(enem$Q039)), ]
+# enem <- enem[!(is.na(enem$Q040)), ]
+# enem <- enem[!(enem$Q042 == '' ), ]
+# enem <- enem[!(enem$Q043 == '' ), ]
+# enem <- enem[!(enem$Q044 == '' ), ]
+# enem <- enem[!(enem$Q045 == '' ), ]
+# enem <- enem[!(enem$Q046 == '' ), ]
+# enem <- enem[!(enem$Q047 == '' ), ]
+# enem <- enem[!(enem$Q048 == '' ), ]
+# enem <- enem[!(enem$Q049 == '' ), ]
+# enem <- enem[!(enem$Q050 == '' ), ]
--- enem/src/R/prepare_dataset.R
+++ enem/src/R/prepare_dataset.R
@@ -1,10 +1,6 @@
 prepare_dataset = function(data) 
 {
-    data[data==""] = NA
-
-    # remove NA NU_IDADE - 
-    # data[is.na(data$NU_IDADE)] <- NULL
-    data <- data[!(is.na(data$TP_SEXO)), ]
+    data <- data[!(is.na(data$NU_IDADE)), ]
 
     # Add default values to missing fields
     data$TP_ESTADO_CIVIL[is.na(data$TP_ESTADO_CIVIL)] = 0           # 'solteiro'
@@ -29,48 +25,17 @@ prepare_dataset = function(data)
 
     data$NU_NOTA_REDACAO[is.na(data$NU_NOTA_REDACAO)] = 0           # essay grade
 
-    data$Q001[is.na(data$Q001)] = 'H'                               # 'nao sei'
-    data$Q002[is.na(data$Q002)] = 'H'                               # 'nao sei'
-    data$Q003[is.na(data$Q003)] = 'F'                               # 'nao sei'
-    data$Q004[is.na(data$Q004)] = 'F'                               # 'nao sei'
-    data$Q005[is.na(data$Q005)] = 0                                 # undefined (new category)
-    data$Q006[is.na(data$Q006)] = 'Z'                               # undefined (new category)
-    data$Q007[is.na(data$Q007)] = 'Z'                               # undefined (new category)
-    data$Q008[is.na(data$Q008)] = 'Z'                               # undefined (new category)
-    data$Q009[is.na(data$Q009)] = 'Z'                               # undefined (new category)
-    data$Q010[is.na(data$Q010)] = 'Z'                               # undefined (new category)
-    data$Q011[is.na(data$Q011)] = 'Z'                               # undefined (new category)
-    data$Q012[is.na(data$Q012)] = 'Z'                               # undefined (new category)
-    data$Q013[is.na(data$Q013)] = 'Z'                               # undefined (new category)
-    data$Q014[is.na(data$Q014)] = 'Z'                               # undefined (new category)
-    data$Q015[is.na(data$Q015)] = 'Z'                               # undefined (new category)
-    data$Q016[is.na(data$Q016)] = 'Z'                               # undefined (new category)
-    data$Q017[is.na(data$Q017)] = 'Z'                               # undefined (new category)
-    data$Q018[is.na(data$Q018)] = 'Z'                               # undefined (new category)
-    data$Q019[is.na(data$Q019)] = 'Z'                               # undefined (new category)
-    data$Q020[is.na(data$Q020)] = 'Z'                               # undefined (new category)
-    data$Q021[is.na(data$Q021)] = 'Z'                               # undefined (new category)
-    data$Q022[is.na(data$Q022)] = 'Z'                               # undefined (new category)
-    data$Q023[is.na(data$Q023)] = 'Z'                               # undefined (new category)
-    data$Q024[is.na(data$Q024)] = 'Z'                               # undefined (new category)
-    data$Q025[is.na(data$Q025)] = 'Z'                               # undefined (new category)
-    data$Q026[is.na(data$Q026)] = 'Z'                               # undefined (new category)
-    data$Q034[is.na(data$Q034)] = 9                                 # undefined (new category)
-    data$Q035[is.na(data$Q035)] = 9                                 # undefined (new category)
-    data$Q036[is.na(data$Q036)] = 9                                 # undefined (new category)
-    data$Q037[is.na(data$Q037)] = 9                                 # undefined (new category)
-    data$Q038[is.na(data$Q038)] = 9                                 # undefined (new category)
-    data$Q039[is.na(data$Q039)] = 9                                 # undefined (new category)
-    data$Q040[is.na(data$Q040)] = 9                                 # undefined (new category)
-    data$Q042[is.na(data$Q042)] = 'Z'                               # undefined (new category)
-    data$Q043[is.na(data$Q043)] = 'Z'                               # undefined (new category)
-    data$Q044[is.na(data$Q044)] = 'Z'                               # undefined (new category)
-    data$Q045[is.na(data$Q045)] = 'Z'                               # undefined (new category)
-    data$Q046[is.na(data$Q046)] = 'Z'                               # undefined (new category)
-    data$Q047[is.na(data$Q047)] = 'Z'                               # undefined (new category)
-    data$Q048[is.na(data$Q048)] = 'Z'                               # undefined (new category)
-    data$Q049[is.na(data$Q049)] = 'Z'                               # undefined (new category)
-    data$Q050[is.na(data$Q050)] = 'Z'                               # undefined (new category)
-    
+    data$Q001[data$Q001 == ''] = 'H'                                # 'nao sei'
+    data$Q002[data$Q002 == ''] = 'H'                                # 'nao sei'
+    data$Q003[data$Q003 == ''] = 'F'                                # 'nao sei'
+    data$Q004[data$Q004 == ''] = 'F'                                # 'nao sei'
+
+    # Remove rows that have too few empty fields
+    # It is not necessary to create a new category for these rows,
+    # we can just remove them as there are just a few
+    # Unnecessary to delete rows for other Qs as deleting the rows for Q005 
+    # already deletes those
+    data <- data[!(is.na(data$Q005)), ]
+
     return(data)
 }
